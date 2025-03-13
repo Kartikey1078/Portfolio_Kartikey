b2503a29217051e603d82e69432965e5b47cc9ff
Kartikey1078 <kartiktyagi002@gmail.com>
2025-03-13 13:53:56 +0530

    facct

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -53,7 +53,7 @@ function Skills() {
               ease: "linear",
             }}
           >
-            {/* DUPLICATING SKILLS LIST FOR INFINITE EFFECT */}
+           
             {[...Array(2)].map((_, i) => (
               <div key={i} className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6 sm:gap-10">
                 {skills.map((skill, index) => (
